2afbd8d84422b69bbcbaca86eea47f1f7163600a
Matome <seletematome3@gmail.com>
2023-04-26 10:09:00 +0200

    first commit

---
+++ src/main/java/com/example/springMongoProject/Controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.springMongoProject.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/v1/student")
+public class StudentController {
+
+    @PostMapping(value = "/save")
+    private String saveStudent(@RequestBody student students) {
+        return 
+    }
+
+
+}
